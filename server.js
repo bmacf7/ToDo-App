fac56976f716beb570b4be43db4f706d32bb8a1a
Byron Machin <bmachin7@gmail.com>
2022-05-20 12:16:34 -0500

    Update item functionality added.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 let express = require("express");
-let { MongoClient } = require("mongodb");
+let { MongoClient, ObjectId } = require("mongodb");
 
 let db;
+let app = express();
 const uri = process.env.MONGO_URI;
+
+app.use(express.static("public"));
 
 async function createConnection() {
   // Create connection to MongoDB database and prepare te server to listen port 3000
@@ -14,8 +17,8 @@ async function createConnection() {
 }
 
 createConnection();
-let app = express();
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
@@ -52,8 +55,8 @@ app.get("/", (req, res) => {
                         <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
                         <span class="item-text">${item.text}</span>
                         <div>
-                            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-                            <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                            <button data-id=${item._id} class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                            <button data-id=${item._id} class="delete-me btn btn-danger btn-sm">Delete</button>
                         </div>
                         </li>
                     `;
@@ -62,7 +65,8 @@ app.get("/", (req, res) => {
                 </ul>
     
             </div>
-  
+            <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+            <script type="text/javascript" src="/main.js"></script>
             </body>
         </html>
     `);
@@ -73,4 +77,16 @@ app.post("/create-item", (req, res) => {
   db.collection("items").insertOne({ text: req.body.item }, () => {
     res.redirect("/");
   });
+});
+
+app.post("/update-item", (req, res) => {
+  // Parse id into ObjectId format for MongoDB.
+  let id = new ObjectId(req.body.id);
+  db.collection("items").findOneAndUpdate(
+    { _id: id },
+    { $set: { text: req.body.text } },
+    () => {
+      res.redirect("/");
+    }
+  );
 });
